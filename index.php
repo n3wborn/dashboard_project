8795cfb1ed8dcfd6bb62c6c7931f452d4d9ce171
lzervini <lezervinia@gmail.com>
2020-07-28 16:23:20 +0200

    Update - Add if result = 0

--- index.php
+++ index.php
@@ -5,13 +5,12 @@ $sql= 'SELECT `ID`,`location`,`name_product`,`ref_product`,`categories`,`purchas
     $sth->execute();
     $result= $sth->fetchAll(PDO::FETCH_ASSOC);
     print_r($result);
-
     $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
 
 
     if (count($result)===0){
-        echo'<p>Il n\'y a aucune donnée à afficher</p>';
+        echo'<p>Rien n\'a encore été rempli</p>';
     }
     
 
