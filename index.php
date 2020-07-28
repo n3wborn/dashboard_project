de7d8175da725fb92297fceae937fec11e8153e3
Stephane Pluchart <n3wborn@pm.me>
2020-07-28 12:11:45 +0200

    Add new twig templates

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require 'php/database.php'; 
+<?php require 'php/database.php';
 
 $sql= 'SELECT `ID`,`location`,`name_product`,`ref_product`,`categories`,`purchase_date`,`garanty_date`,`price`,`advice`,`picture`,`manual` FROM `achat_materiel` ';
     $sth= $dbh->prepare($sql);
@@ -12,8 +12,25 @@ $sql= 'SELECT `ID`,`location`,`name_product`,`ref_product`,`categories`,`purchas
     if (count($result)===0){
         echo'<p>Rien n\'a encore été rempli</p>';
     }
-    
 
 
-?>
 
+
+
+/* TWIG */
+/* Variables */
+$project_title = 'Dashboard Project';
+
+
+/* Conf */
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+/* Templates */
+$template = $twig->load('index.html');
+echo $template->render(['project_title' => $project_title]);
