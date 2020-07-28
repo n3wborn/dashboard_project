40d1c0681b4269f4b4880379df9506cac62cd672
lzervini <lezervinia@gmail.com>
2020-07-28 15:49:26 +0200

    Update - Add request sql SELECT

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php require 'php/database.php'; 
 
-$sql= 'SELECT * FROM category';
+$sql= 'SELECT `ID`,`location`,`name_product`,`ref_product`,`categories`,`purchase_date`,`garanty_date`,`price`,`advice`,`picture`,`manual` FROM `achat_materiel` ';
     $sth= $dbh->prepare($sql);
     $sth->execute();
     $result= $sth->fetchAll(PDO::FETCH_ASSOC);
     print_r($result);
+
+    $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+
+
+
+    if (count($result)===0){
+        echo'<p>Il n\'y a aucune donnée à afficher</p>';
+    }
+    
+
 
 ?>
 
