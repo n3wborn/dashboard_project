60d66ba9865f58cb77ff6dd82fc5198646941a3f
lzervini <lezervinia@gmail.com>
2020-07-28 08:58:40 +0200

    Update - change require_once -> require

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require 'php/database.php'; 
+
+$sql= 'SELECT * FROM category';
+    $sth= $dbh->prepare($sql);
+    $sth->execute();
+    $result= $sth->fetchAll(PDO::FETCH_ASSOC);
+    print_r($result);
+?>
+
