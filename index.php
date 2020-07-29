bab14fa98da6402d847fc4bc2899600734586468
lzervini <lezervinia@gmail.com>
2020-07-29 11:49:57 +0200

    Update -change echo

--- index.php
+++ index.php
@@ -4,16 +4,13 @@ $sql= 'SELECT `ID`,`location`,`name_product`,`ref_product`,`categories`,`purchas
     $sth= $dbh->prepare($sql);
     $sth->execute();
     $result= $sth->fetchAll(PDO::FETCH_ASSOC);
-    print_r($result);
     $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
 
 
     if (count($result)===0){
-        echo'<p>Rien n\'a encore été rempli</p>';
+        echo'<p>Aucune données n\'a été rentrées</p>';
     }
-
-
 
 
 
