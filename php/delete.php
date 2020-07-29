80438cb4f81f1c9e0d42cc0369d95de1278ca7d1
lzervini <lezervinia@gmail.com>
2020-07-29 11:48:27 +0200

    Update - Add request to delete

--- php/delete.php
+++ php/delete.php
@@ -2,7 +2,10 @@
 
 if(isset($_POST['id'])){
     $sql=' DELETE FROM achat_materiel WHERE id=:id '
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+    $sth -> execute();
 }
 
-
+header('Location: ../index.php');
 ?>
