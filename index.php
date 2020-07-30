9f7f843a0ede08ea7f4dcd541e455500edbd9264
lzervini <lezervinia@gmail.com>
2020-07-30 13:44:17 +0200

    Update - Request works

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php require 'php/database.php';
 
 
+$sql= 'SELECT achat_materiel.ID, s.name AS place , name_product,ref_product,c.name AS name,purchase_date,garanty_date,price,advice,`picture`,`manual` FROM achat_materiel INNER JOIN category AS c ON achat_materiel.categories=c.id INNER JOIN sites AS s ON achat_materiel.location=s.id ' ;
 
-$sql= 'SELECT achat_materiel.ID,location,name_product,ref_product,c.name AS name,purchase_date,garanty_date,price,advice,`picture`,`manual` FROM achat_materiel INNER JOIN category AS c ON achat_materiel.categories=c.id' ;
     $sth= $dbh->prepare($sql);
-
+    
     $sth->execute();
     $result= $sth->fetchAll(PDO::FETCH_ASSOC);
     $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
-
+    
 
     if (count($result)===0){
         echo'<p>Aucune données n\'a été rentrées</p>';
