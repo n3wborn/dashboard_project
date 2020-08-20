ed032ab6444074a2d7026c267e95cf5cc4445145
Stephane Pluchart <n3wborn@pm.me>
2020-08-20 10:43:59 +0200

    Fix index.php location when redirected

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php require_once 'php/database.php';
-require_once "php/functions.php"; 
+require_once "php/functions.php";
 
 if (connected()) {
 	header('Location: ./index.php');
@@ -13,12 +13,12 @@ if(count($_POST) > 0) {
 	$sth = $dbh->prepare($sql);
 	$sth->bindParam(':user', $user, PDO::PARAM_STR);
 	$sth->execute();
-    $result = $sth -> fetch();
-    
+	$result = $sth -> fetch();
+
 
 	if ($user === $result["user"] && pwd_verify($password, $result["password"])) {
-		header('Location: ../index.php');
+		header('Location: ./index.php');
 	} else {
 		echo "Utilisateur ou mot de passe vide";
 	}
-	} 
+}
