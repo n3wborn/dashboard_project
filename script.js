eae87d4a7ee363aa3ed183d5e67a6c1a02468a10
Stephane Pluchart <n3wborn@pm.me>
2020-08-23 19:38:25 +0200

    Refactor - upgrade js elements deletion scripts

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-// select needed elements
+// Elements  we will have to look for
 const deleteLinks = document.getElementsByClassName('dellinks');
 const noBtn = document.getElementById('modal-btn-no');
 const yesBtn = document.getElementById('modal-btn-yes');
+const modal = document.getElementById('modal');
+
+// functions needed
+function removeReadyToDelete() {
+	const elementsToDelete = document.getElementsByClassName('readyToDelete')
+	for (elementToDelete of elementsToDelete) {
+		elementToDelete.classList.toggle('readyToDelete')
+	}
+}
 
 
-// For each delete links
+// Keep an eye on delete links
+// if click, show modal and permit element deletion
 for (deleteLink of deleteLinks) {
-	// toogle hidden and readyToDelete classes
 	deleteLink.addEventListener('click', function(e){
 		e.preventDefault();
-		const modal = document.getElementById('modal');
-		modal.classList.toggle('hidden');
 
+		modal.classList.toggle('hidden');
 		this.classList.toggle('readyToDelete');
 	})
 }
 
 
-// Do not confirm deletion if clicked on modal noBtn
+// When element are ready to  be deleted
+// Avoid element deletion if clicked on noBtn
 noBtn.addEventListener('click', function() {
-		const modal = document.getElementById('modal');
-		modal.classList.toggle('hidden');
-		const elementsToDelete = document.getElementsByClassName('readyToDelete')
+	modal.classList.toggle('hidden');
 
-		for (elementToDelete of elementsToDelete) {
-			elementToDelete.classList.toggle('readyToDelete')
-		}
+	// and remove class permitting deletion
+	removeReadyToDelete();
 })
 
 
+// When element are ready to  be deleted
+// Act as noBtn
+modal.addEventListener('click', function() {
+	if (!modal.classList.contains('hidden')) {
+		modal.classList.toggle('hidden');
+		removeReadyToDelete();
+	}
+})
+
+
+// When element are ready to  be deleted
 // Confirm deletion if clicked on modal yesBtn
 yesBtn.addEventListener('click', function() {
 	const elementsToDelete = document.getElementsByClassName('readyToDelete')
