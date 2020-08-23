d08db18d1e4b065253eefd1c1d186b675d61ff68
Stephane Pluchart <n3wborn@pm.me>
2020-08-23 20:38:52 +0200

    Remove commented connection cond - delete.php

--- delete.php
+++ delete.php
@@ -1,9 +1,10 @@
 <?php require_once 'php/database.php';
-require_once "php/functions.php"; 
+require_once "php/functions.php";
 
-// if (!connected()) {
-// 	header('Location: ./login.php');
-// }
+if (!connected()) {
+	header('Location: ./login.php');
+}
+
 if(isset($_GET['id'])){
     $sql = 'DELETE FROM achat_materiel WHERE id=:id';
     $sth = $dbh->prepare($sql);
